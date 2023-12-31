2b543ff117469d4ad7e5e4be8cba005122cb7afa
Evan <41494790+evaan@users.noreply.github.com>
2023-12-31 04:51:06 -0330

    Unoptimized /all

--- API/API.go
+++ API/API.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/fatih/structs"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
@@ -14,85 +15,111 @@ var db *sql.DB
 var logger *log.Logger
 var err error
 
+type Subject struct {
+	Name         string `json:"name"`
+	FriendlyName string `json:"friendlyName"`
+}
+
 type Course struct {
-	crn        string
-	id         string
-	name       string
-	section    string
-	dateRange  any
-	courseType any
-	instructor any
-	subject    string
-	campus     string
-	comment    any
-	credits    int
-	semester   int
+	Crn        string `json:"crn"`
+	Id         string `json:"id"`
+	Name       string `json:"name"`
+	Section    string `json:"section"`
+	DateRange  any    `json:"dateRange"`
+	CourseType any    `json:"type"`
+	Instructor any    `json:"instructor"`
+	Subject    string `json:"subject"`
+	Campus     string `json:"campus"`
+	Comment    any    `json:"comment"`
+	Credits    int    `json:"credits"`
+	Semester   int    `json:"semester"`
 }
 
 type Time struct {
-	crn       string
-	days      string
-	startTime string
-	endTime   string
-	location  string
+	Crn       string `json:"crn"`
+	Days      string `json:"days"`
+	StartTime string `json:"startTime"`
+	EndTime   string `json:"endTime"`
+	Location  string `json:"location"`
+}
+
+type Seating struct {
+	Crn       string `json:"crn"`
+	Available string `json:"available"`
+	Max       string `json:"max"`
+	Waitlist  string `json:"waitlist"`
+	Checked   string `json:"checked"`
 }
 
 func all(w http.ResponseWriter, r *http.Request) {
-	rows, err := db.Query("SELECT * FROM courses")
-	if err != nil {
-		logger.Fatal(err)
-	}
-	defer rows.Close()
-
 	output := make(map[string][]any)
 
-	for rows.Next() {
-		var course Course
-		tmp := make(map[string]any)
-
-		err := rows.Scan(&course.crn, &course.id, &course.name, &course.section, &course.dateRange, &course.courseType, &course.instructor, &course.subject, &course.campus, &course.comment, &course.credits, &course.semester)
-		if err != nil {
-			logger.Fatal(err)
-		}
-
-		tmp["crn"] = course.crn
-		tmp["id"] = course.id
-		tmp["name"] = course.name
-		tmp["section"] = course.section
-		tmp["dateRange"] = course.dateRange
-		tmp["type"] = course.courseType
-		tmp["instructor"] = course.instructor
-		tmp["subject"] = course.subject
-		tmp["campus"] = course.campus
-		tmp["comment"] = course.comment
-		tmp["credits"] = course.credits
-		tmp["semester"] = course.semester
-
-		output["courses"] = append(output["courses"], tmp)
-	}
-
-	rows, err = db.Query("SELECT times.crn, times.days, times.\"startTime\", times.\"endTime\", times.location FROM times")
+	subjects, err := db.Query("SELECT * FROM subjects")
 	if err != nil {
 		logger.Fatal(err)
 	}
-	defer rows.Close()
+	defer subjects.Close()
 
-	for rows.Next() {
-		var time Time
-		tmp := make(map[string]any)
+	for subjects.Next() {
+		var subject Subject
 
-		err := rows.Scan(&time.crn, &time.days, &time.startTime, &time.endTime, &time.location)
+		err := subjects.Scan(&subject.Name, &subject.FriendlyName)
 		if err != nil {
 			logger.Fatal(err)
 		}
 
-		tmp["crn"] = time.crn
-		tmp["days"] = time.days
-		tmp["startTime"] = time.startTime
-		tmp["endTime"] = time.endTime
-		tmp["location"] = time.location
+		output["subjects"] = append(output["subjects"], structs.Map(subject))
+	}
 
-		output["times"] = append(output["times"], tmp)
+	courses, err := db.Query("SELECT * FROM courses")
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer courses.Close()
+
+	for courses.Next() {
+		var course Course
+
+		err := courses.Scan(&course.Crn, &course.Id, &course.Name, &course.Section, &course.DateRange, &course.CourseType, &course.Instructor, &course.Subject, &course.Campus, &course.Comment, &course.Credits, &course.Semester)
+		if err != nil {
+			logger.Fatal(err)
+		}
+
+		output["courses"] = append(output["courses"], structs.Map(course))
+	}
+
+	times, err := db.Query("SELECT times.crn, times.days, times.\"startTime\", times.\"endTime\", times.location FROM times")
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer times.Close()
+
+	for times.Next() {
+		var time Time
+
+		err := times.Scan(&time.Crn, &time.Days, &time.StartTime, &time.EndTime, &time.Location)
+		if err != nil {
+			logger.Fatal(err)
+		}
+
+		output["times"] = append(output["times"], structs.Map(time))
+	}
+
+	seatings, err := db.Query("SELECT * FROM seatings")
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer seatings.Close()
+
+	for seatings.Next() {
+		var seating Seating
+
+		err := seatings.Scan(&seating.Crn, &seating.Available, &seating.Max, &seating.Waitlist, &seating.Checked)
+		if err != nil {
+			logger.Fatal(err)
+		}
+
+		output["seatings"] = append(output["seatings"], structs.Map(seating))
 	}
 
 	jsonString, err := json.Marshal(output)
@@ -100,6 +127,7 @@ func all(w http.ResponseWriter, r *http.Request) {
 		logger.Fatal(err)
 	}
 
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(jsonString))
 }
