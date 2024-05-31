cb7c4763327f62e4ce2f35a869243dcd7243ae6a
Evan <41494790+evaan@users.noreply.github.com>
2024-05-31 14:54:42 -0230

    Change colour scheme

--- ScheduleBuilder/src/components/SectionButton.tsx
+++ ScheduleBuilder/src/components/SectionButton.tsx
@@ -14,7 +14,7 @@ export function SectionButton(props: {section: Course}) {
 
     return (
         <div>
-            <Button variant={props.section.type == "Laboratory" ? "outline-primary" : "primary"} style={{width: "100%", marginBottom: "4px"}} onClick={async () => {setModalOpen(true);}}>
+            <Button variant={props.section.type == "Laboratory" ? "outline-primary" : "primary"} style={{width: "100%", marginBottom: "4px", color: "#fff"}} onClick={async () => {setModalOpen(true);}}>
                 {props.section.dateRange !== null ? `${props.section.type} - ${tmp} ${!props.section.type.includes("Laboratory") ? "- " + props.section.instructor : ""}` : `No Information, Section: ${props.section.section}`}
             </Button>
             <SectionModal isOpen={modalOpen} onHide={closeModal} section={props.section}/>
