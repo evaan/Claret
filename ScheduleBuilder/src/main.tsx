5f7d32f7f44f3299809c5405e2f67d76cc9dbe81
Evan <41494790+evaan@users.noreply.github.com>
2024-05-31 11:03:15 -0230

    Change font and header colour

--- ScheduleBuilder/src/main.tsx
+++ ScheduleBuilder/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "@fontsource/sen/latin.css";
+import "@fontsource/montserrat/latin.css";
 import "./styles.css";
 import App from "./App";
 
